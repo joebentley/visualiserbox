ffe580d111c40d6bee7b3a098b745fb4b8cca685
Joe Bentley <joebentley10@gmail.com>
2025-08-22 10:25:52 +0200

    Started writing stack based language for pixel manipuation
    Added basic ^ and + operators. Still need to come up with a design for
    preventing exhausting the stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod program;
+
 use raylib::prelude::*;
 
 fn path_relative_to_executable(font_file_name: &str) -> std::path::PathBuf {
@@ -35,25 +37,27 @@ fn main() {
 
         for y in 0..scaled_height {
             for x in 0..scaled_width {
-                let x_val = (2.0 * (x as f64) * (4.0 + 2.0 * f64::sin(t))) as i32;
-                let y_val = (1.0 * (y as f64) * (6.0 + 2.0 * f64::sin(t + 0.2))) as i32;
+                let (h, s, v) = program::execute_string(input.as_str(), x, y, t);
+
+                // let x_val = (2.0 * (x as f64) * (4.0 + 2.0 * f64::sin(t))) as i32;
+                // let y_val = (1.0 * (y as f64) * (6.0 + 2.0 * f64::sin(t + 0.2))) as i32;
                 d.draw_rectangle(
                     x * scale,
                     y * scale,
                     scale,
                     scale,
-                    Color::color_from_hsv(f64::cos(t) as f32, 1.0, (x_val ^ y_val) as f32),
+                    Color::color_from_hsv(h, s, v),
                 );
             }
         }
 
         if let Some(c) = d.get_char_pressed() {
-            input.push(c);
+            input = c.to_string() + &input;
         } else if !input.is_empty()
             && (d.is_key_pressed(KeyboardKey::KEY_BACKSPACE)
                 || d.is_key_pressed_repeat(KeyboardKey::KEY_BACKSPACE))
         {
-            input.pop();
+            input = input[1..].to_string();
         }
 
         d.draw_text_ex(
---
+++ src/program.rs
@@ -0,0 +1,52 @@
+struct Stack {
+    stack: Vec<f32>,
+}
+
+impl Stack {
+    fn new() -> Stack {
+        Stack { stack: Vec::new() }
+    }
+
+    fn push(&mut self, val: f32) {
+        self.stack.push(val);
+    }
+
+    fn pop(&mut self) -> f32 {
+        self.stack.pop().unwrap_or(0.0)
+    }
+
+    fn pop2(&mut self) -> (f32, f32) {
+        (self.pop(), self.pop())
+    }
+
+    fn pop_or(&mut self, default: f32) -> f32 {
+        self.stack.pop().unwrap_or(default)
+    }
+
+    fn execute(&mut self, instruction: char) {
+        match instruction {
+            '^' => {
+                let (a, b) = self.pop2();
+                self.push(((a as i32) ^ (b as i32)) as f32);
+            }
+            '+' => {
+                let val = self.pop() + self.pop();
+                self.push(val);
+            }
+            _ => {}
+        }
+    }
+}
+
+pub fn execute_string(input: &str, x: i32, y: i32, t: f64) -> (f32, f32, f32) {
+    let mut stack = Stack::new();
+    stack.push(t as f32);
+    stack.push(y as f32);
+    stack.push(x as f32);
+
+    for c in input.chars().rev() {
+        stack.execute(c);
+    }
+
+    (stack.pop(), stack.pop(), stack.pop_or(1.0))
+}
