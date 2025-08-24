dd2da773208ddc49ca4c547e651e7c63bac62736
Joe Bentley <joebentley10@gmail.com>
2025-08-24 12:32:47 +0200

    Use threading for image saving
    Raylib's image saving function is a bit slow and the ffi Image type
    cannot be sent across thread boundaries. To parallelise the image
    saving, it grabs the ImageColors from the Raylib image, and then in
    the threads it converts to image::RgbImage and saves them as
    PNGs. This seems to give a minor (but unmeasured) speed improvement

--- src/recorder.rs
+++ src/recorder.rs
@@ -1,6 +1,26 @@
-use raylib::texture::Image;
+use std::thread::JoinHandle;
+
+use raylib::texture::{Image, ImageColors};
 
 use crate::ringbuffer::RingBuffer;
+
+struct ImageData {
+    colors: ImageColors,
+    width: u32,
+    height: u32,
+}
+
+impl ImageData {
+    fn to_rgbimage(&self) -> image::RgbImage {
+        let mut rgb_image = image::RgbImage::new(self.width, self.height);
+        for (i, color) in self.colors.iter().enumerate() {
+            let x = i % (self.width as usize);
+            let y = i / (self.width as usize);
+            rgb_image.put_pixel(x as u32, y as u32, image::Rgb([color.r, color.g, color.b]));
+        }
+        rgb_image
+    }
+}
 
 pub struct ScreenRecorder {
     frames: RingBuffer<Image>,
@@ -17,16 +37,41 @@ impl ScreenRecorder {
         self.frames.push(image)
     }
 
+    fn convert_frames_to_imagedata(&self) -> Vec<ImageData> {
+        let mut image_datas = Vec::new();
+
+        for frame in self.frames.into_iter() {
+            image_datas.push(ImageData {
+                colors: frame.get_image_data(),
+                width: frame.width as u32,
+                height: frame.height as u32,
+            });
+        }
+
+        image_datas
+    }
+
     pub fn save_as_video(&self, filepath: &str) {
         let tmp_dir = std::env::temp_dir();
         // Concatenate a UUID to ensure there is no clash with other
         // pngs already in the tmp_dir
         let uuid = uuid::Uuid::new_v4();
 
-        for (i, frame) in self.frames.into_iter().enumerate() {
+        let mut handles: Vec<JoinHandle<_>> = Vec::new();
+        let image_datas = self.convert_frames_to_imagedata();
+        for (i, image_data) in image_datas.into_iter().enumerate() {
             let mut frame_path = tmp_dir.clone();
             frame_path.push(format!("frame_{}_{}.png", uuid, i));
-            frame.export_image(frame_path.to_str().unwrap());
+            let handle = std::thread::spawn(move || {
+                let frame = image_data.to_rgbimage();
+                frame.save(frame_path.to_str().unwrap()).unwrap();
+                println!("Saving frame {}", i);
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
         }
 
         let mut frame_glob = tmp_dir.clone();
