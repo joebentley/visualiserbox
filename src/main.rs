998cb46765caa58280e52cd6ba530de79e5f7a92
Joe Bentley <joebentley10@gmail.com>
2025-08-24 11:19:29 +0200

    Seperate input logic and drawing logic
    rl.begin_drawing() corresponds to BeginDrawing() in Raylib. It is
    moved to a scope to contain drawing operations and separated from
    input handling logic

--- src/main.rs
+++ src/main.rs
@@ -30,68 +30,71 @@ fn main() {
         )
         .unwrap();
 
+
     while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-        let t = d.get_time();
+        let t = rl.get_time();
 
-        d.clear_background(Color::WHITE);
-
-        let mouse_position = d.get_mouse_position();
+        let mouse_position = rl.get_mouse_position();
         let (mx, my) = (
             mouse_position.x.floor() as i32 / scale,
             mouse_position.y.floor() as i32 / scale,
         );
 
-        let mut debug_string = String::new();
-
-        for y in 0..scaled_height {
-            for x in 0..scaled_width {
-                let mut stack = program::execute_string(input.as_str(), x, y, t);
-
-                if y == my && x == mx {
-                    for value in stack.get_stack() {
-                        debug_string += (value.to_string() + "\n").as_str();
-                    }
-                    debug_string += format!("{} {}", mx, my).as_str();
-                }
-
-                let (h, s, v) = (stack.pop(), stack.pop(), stack.pop());
-
-                d.draw_rectangle(
-                    x * scale,
-                    y * scale,
-                    scale,
-                    scale,
-                    Color::color_from_hsv(h, s, v),
-                );
-            }
-        }
-
-        d.draw_text_ex(
-            &font,
-            debug_string.as_str(),
-            Vector2::new(width as f32 - 150.0, 20.0),
-            30.0,
-            0.0,
-            Color::NAVAJOWHITE,
-        );
-
-        if let Some(c) = d.get_char_pressed() {
-            input = c.to_string() + &input;
+        if let Some(c) = rl.get_char_pressed() {
+                input = c.to_string() + &input;
         } else if !input.is_empty()
-            && (d.is_key_pressed(KeyboardKey::KEY_BACKSPACE)
-                || d.is_key_pressed_repeat(KeyboardKey::KEY_BACKSPACE))
+            && (rl.is_key_pressed(KeyboardKey::KEY_BACKSPACE)
+                || rl.is_key_pressed_repeat(KeyboardKey::KEY_BACKSPACE))
         {
             input = input[1..].to_string();
         }
 
-        d.draw_text_ex(
-            &font,
-            input.as_str(),
-            Vector2::new(20.0, 20.0),
-            40.0,
-            0.0,
-            Color::NAVAJOWHITE,
-        );
+        {
+            let mut d = rl.begin_drawing(&thread);
+            d.clear_background(Color::WHITE);
+
+            let mut debug_string = String::new();
+
+            for y in 0..scaled_height {
+                for x in 0..scaled_width {
+                    let mut stack = program::execute_string(input.as_str(), x, y, t);
+
+                    if y == my && x == mx {
+                        for value in stack.get_stack() {
+                            debug_string += (value.to_string() + "\n").as_str();
+                        }
+                        debug_string += format!("{} {}", mx, my).as_str();
+                    }
+
+                    let (h, s, v) = (stack.pop(), stack.pop(), stack.pop());
+
+                    d.draw_rectangle(
+                        x * scale,
+                        y * scale,
+                        scale,
+                        scale,
+                        Color::color_from_hsv(h, s, v),
+                    );
+                }
+            }
+            d.draw_text_ex(
+                &font,
+                debug_string.as_str(),
+                Vector2::new(width as f32 - 150.0, 20.0),
+                30.0,
+                0.0,
+                Color::NAVAJOWHITE,
+            );
+
+            d.draw_text_ex(
+                &font,
+                input.as_str(),
+                Vector2::new(20.0, 20.0),
+                40.0,
+                0.0,
+                Color::NAVAJOWHITE,
+            );
+        }
+
     }
 }
