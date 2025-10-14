3262621128236a86fca7e4765c515918c1332a57
Joe Bentley <joebentley10@gmail.com>
2025-10-14 10:10:22 +0200

    Run rustfmt on utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,9 @@
-pub fn map(input_start: f32, input_end: f32, output_start: f32, output_end: f32, input: f32) -> f32 {
+pub fn map(
+    input_start: f32,
+    input_end: f32,
+    output_start: f32,
+    output_end: f32,
+    input: f32,
+) -> f32 {
     output_start + ((output_end - output_start) * (input - input_start)) / (input_end - input_start)
 }
